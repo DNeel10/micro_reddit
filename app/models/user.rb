f940a19aaa92ffb9c6e6113ef65a41d1f50f755f
Daniel Neel <dneel10@gmail.com>
2023-05-15 22:31:27 -0400

    create and update user and post models and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length: { in: 4...12 }, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { in: 8...20 }
+
+    has_many :posts
+
 end
